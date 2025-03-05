34c4b7b14d4e1a0a601acaf2289ea2370d556d4c
aliaksxssv <alexey.krasnou@gmail.com>
2025-03-05 13:18:23 +0100

    github pages

--- docker/src/07 Defense Evasion/malicious.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-// This function will execute when the shared library is loaded
-__attribute__((constructor))
-void run_on_load() {
-    printf("\n�~_~T� You are hacked! �~_~T�\n");
-}
